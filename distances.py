71e633c125d15f25b4627172ae92b9dd4753ddef
pdbruno <pdbruno@gmail.com>
2024-07-21 23:21:15 -0300

    corro experimento para todas las distancias

--- distances.py
+++ distances.py
@@ -1,14 +1,29 @@
 from numpy import linalg as LA
 import math
 
+
 def trace_distance(r, s):
-    return LA.vector_norm(r-s)
+    return LA.vector_norm(r - s)
+
 
 def fidelity(r, s):
-    return (1 + r @ s)/2
+    return (1 + r @ s) / 2
+
 
 def affinity(r, s):
-    return (r @ s + (1+math.sqrt(1-(r @ r)))(1+math.sqrt(1-(s @ s))))/((math.sqrt(1+LA.vector_norm(r))+math.sqrt(1-LA.vector_norm(r)))*(math.sqrt(1+LA.vector_norm(s))+math.sqrt(1-LA.vector_norm(s))))
+    return (
+        r @ s + (1 + math.sqrt(1 - min(1, r @ r))) * (1 + math.sqrt(1 - min(1, s @ s)))
+    ) / (
+        (
+            math.sqrt(1 + min(1, LA.vector_norm(r)))
+            + math.sqrt(1 - min(1, LA.vector_norm(r)))
+        )
+        * (
+            math.sqrt(1 + min(1, LA.vector_norm(s)))
+            + math.sqrt(1 - min(1, LA.vector_norm(s)))
+        )
+    )
+
 
 def wooters_distance(r, s):
     return math.acos(math.sqrt(fidelity(r, s)))
